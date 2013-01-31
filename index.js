e205ef5704c34e62ea88557e61e581e593afe3d6
goldquest <tjwelde@gmail.com>
2013-01-31 16:19:52 +0100

    working regexp version

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var eejs = require('ep_etherpad-lite/node/eejs/');
 exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb) {
 	var str = args.content;
 	str = str.replace(/\n/gm, "<newline>");
-	console.log(str);
-	str = str.replace(/<li data-key="embed">.*<\/li>/, "");
+	str = str.replace(/<li data-key="embed">.*<li class="separator">/, "<li class=\"separator\">");
 	str = str.replace(/<newline>/gm, '\n');
+	str = str.replace(/class="grouped-left" id="settingslink"/, "id=\"settingslink\"");
+	console.log(str);
 	args.content = str;
   return cb();
 };
